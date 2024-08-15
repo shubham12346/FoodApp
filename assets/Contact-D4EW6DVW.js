e9df1c941b73ed4b3c70e5d31196540a671fb05e
shubham agrahari <shubham.agrahari@indexnine.com>
2024-08-15 17:37:06 +0530

    Updates

---
+++ assets/Contact-D4EW6DVW.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DGNI-GqY.js";const o=()=>t.jsx("div",{style:{marginTop:"200px"},children:"This is contact page "});export{o as default};
--- assets/Contact-rfxlRSSN.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BWo4AN8e.js";const o=()=>t.jsx("div",{style:{marginTop:"200px"},children:"This is contact page "});export{o as default};
