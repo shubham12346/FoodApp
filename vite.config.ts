7d570f264f1712deba0f44e05947f2139bb12bf8
shubham agrahari <shubham.agrahari@indexnine.com>
2024-08-15 12:04:08 +0530

    Deploy the page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import tsconfigPaths from "vite-tsconfig-paths";
+// import tsconfigPaths from "vite-tsconfig-paths";
 import Pages from "vite-plugin-pages";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), Pages()],
+  plugins: [react(), Pages()],
   base: "/FoodApp.github.io/",
 });
